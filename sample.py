e7a0911a093538e51fd0b43c8046142b5ab6becf
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-27 21:52:54 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,11 @@
+import os, time
+
+path = r"D:\file"
+now = time.time()
+
+for filename in os.listdir(path):
+    
+    if os.path.getmtime(os.path.join(path, filename)) < now - 7 * 86400:
+        if os.path.isfile(os.path.join(path, filename)):
+            print(filename)
+            os.remove(os.path.join(path, filename))
