36cd0191bf6b13858908ae68fd6e11f8480fae42
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-27 22:48:27 +0530

    Delete easy.py

--- easy.py
+++
@@ -1,6 +0,0 @@
-import os
-
-directory = 'file/Boot/Camera Roll/Microsoft/Saved Pictures/Screenshots/cpp'
-
-os.system("find " + directory + " -mtime +60 -print")
-os.system("find " + directory + " -mtime +60 -delete")
