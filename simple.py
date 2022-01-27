bd688adaf5d9979abf3ae58185b6070433cecb63
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-27 22:30:07 +0530

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,6 @@
+import os
+from time import time
+
+p='.'
+result=[os.remove(file) for file in (os.path.join(path, file) for path, _, files in os.walk(p) for file in files) if os.stat(file).st_mtime < time() - 7 * 86400]
+print(result)
