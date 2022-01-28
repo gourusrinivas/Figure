b668fc2298fbb1d3f0aaf292299422cc450e8822
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-28 16:58:12 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -9,4 +9,18 @@ for filename in os.listdir(path):
         if os.path.isfile(os.path.join(path, filename)):
             print(filename)
             os.remove(os.path.join(path, filename))
-    
+            
+import os
+import zipfile
+
+def zipdir(path, ziph):
+# ziph is zipfile handle
+for root, dirs, files in os.walk(path):
+for file in files:
+ziph.write(os.path.join(root, file),
+os.path.relpath(os.path.join(root, file),
+os.path.join(path, '..')))
+
+zipf = zipfile.ZipFile('Other.zip', 'w', zipfile.ZIP_DEFLATED)
+zipdir('tmp/', zipf)
+zipf.close()
