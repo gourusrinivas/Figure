e0817826f2ea7b83c36fea72909308bfe859f226
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-28 11:33:40 +0530

    Delete simple.py

--- simple.py
+++
@@ -1,6 +0,0 @@
-import os
-from time import time
-
-p='D:\file'
-result=[os.remove(file) for file in (os.path.join(path, file) for path, _, files in os.walk(p) for file in files) if os.stat(file).st_mtime < time() - 7 * 86400]
-print(result)
