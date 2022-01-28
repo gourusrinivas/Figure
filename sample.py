0e29cd14b261d598957019c25e25030b8784f5f9
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-28 13:12:09 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,7 +5,7 @@ now = time.time()
 
 for filename in os.listdir(path):
     
-    if os.path.getmtime(os.path.join(path, filename)) < now - 7 * 86400:
+    if os.path.getmtime(os.path.join(path, filename)) < now - 2 * 86400:
         if os.path.isfile(os.path.join(path, filename)):
             print()
             os.remove(os.path.join(path, filename))
